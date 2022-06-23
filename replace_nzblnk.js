22464411820403c25a4cb148b4b63676c01db8a7
andre1808 <41615984+andre1808@users.noreply.github.com>
2022-06-23 11:01:09 +0200

    Create replace_nzblnk.js

---
+++ replace_nzblnk.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         replace nzblnk links
+// @description  replace nzblnk with shortcut links"
+// @grant       none
+// ==/UserScript==
+
+var links = document.getElementsByTagName( 'a' );
+
+  
+for ( var i = 0; i < links.length; i++ ) {
+    var link = links[i];
+   link = links[i];
+   link.href = link.href.replace(/^(nzblnk:)([^&]+)&([^&]+)&(.+)$/gi, 'shortcuts://run-shortcut?name=NZBLink&input=text&text=$1$2$$$3$$$4');
+}
+;
